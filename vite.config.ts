6009dcd960cdd924d4d63c9e750438f7c153c1d7
Irfan IR <ir.358.786@gmail.com>
2025-07-03 14:06:15 +0530

    set base path for GH pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/Protfolio_web_ir/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
